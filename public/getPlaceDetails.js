5e43489b7f90d8259e6363018bc0398ed573678f
Andres Rodriguez <andresrod21@gmail.com>
2017-12-25 15:22:12 +0000

    Fixed issue with counties in geolocateUser

--- public/getPlaceDetails.js
+++ public/getPlaceDetails.js
@@ -5,6 +5,7 @@ function getPlaceDetails(recordId, postalCode) {
             const placeId = response[0].place_id;
             service.getDetails({placeId}, function (placeDetails, status) {
                 if (status == google.maps.places.PlacesServiceStatus.OK) {
+                    console.log('Place details:', placeDetails);
                     const element = document.createElement('div');
                     element.id = 'placeDetails';
                     element.style.visibility = 'hidden';
--- src/utils/geolocateUser.js
+++ src/utils/geolocateUser.js
@@ -11,15 +11,22 @@ export default (recordId, placeDetails) => {
 
     const lat = placeDetails.geometry.location.lat;
     const lng = placeDetails.geometry.location.lng;
-    const country = addressComponents.find((component) => component.types[0] === 'country').short_name;
-    const state = addressComponents.find((component) => component.types[0] === 'administrative_area_level_1').short_name.trim();
-    const city = addressComponents.find((component) => component.types[0] === 'locality' || component.types[0] === 'postal_town').long_name.trim();
-
-    let county = addressComponents.find((component) => component.types[0] === 'administrative_area_level_2').long_name;
-    if (county.substr(county.length-6) === "County") {  
-        county = county.substr(0,county.length-7);                                  //STRIP "County" FROM STRING
-    } else if (!county && city) {
-        county = city;
+    const country = addressComponents.find((component) => component.types[0] === 'country').short_name;     //Two-letter identifier
+    
+    let state = addressComponents.find((component) => component.types[0] === 'administrative_area_level_1');
+    state = state && state.short_name.trim();    //Two-letter identifier
+    
+    let city = addressComponents.find((component) => component.types[0] === 'locality' || component.types[0] === 'postal_town');     
+    city = city && city.long_name.trim();   //Full name
+    
+    let county = addressComponents.find((component) => component.types[0] === 'administrative_area_level_2');     
+    if (county) {  
+        county = county.long_name;
+        if (county.substr(county.length-6) === "County") {              //IF "County" FOUND IN NAME...
+            county = county.substr(0,county.length-7);                  //STRIP "County" FROM STRING
+        }
+    } else if (!county && city) {                                       //IF COUNTY NOT FOUND...
+        county = city;                                                  //SET EQUAL TO CITY
     }
     county = county.trim();
 
